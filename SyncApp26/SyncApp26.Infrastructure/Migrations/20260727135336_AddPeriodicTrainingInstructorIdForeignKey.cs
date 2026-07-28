c7106f934b2f4b4ef251dad1d1ee392bf5eb3490
Karina-Melissa Constandache <karinaconstandache12@gmail.com>
2026-07-28 10:18:21 +0300

    fix: migration errors

---
+++ SyncApp26/SyncApp26.Infrastructure/Migrations/20260727135336_AddPeriodicTrainingInstructorIdForeignKey.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SyncApp26.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPeriodicTrainingInstructorIdForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddForeignKey(
+                name: "FK_PeriodicTrainings_Users_InstructorId",
+                table: "PeriodicTrainings",
+                column: "InstructorId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PeriodicTrainings_Users_InstructorId",
+                table: "PeriodicTrainings");
+        }
+    }
+}
