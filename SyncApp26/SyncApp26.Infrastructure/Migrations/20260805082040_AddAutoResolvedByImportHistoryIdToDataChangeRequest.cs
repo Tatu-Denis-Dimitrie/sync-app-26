a36ca961f2fbdff725b30e5afc709b1b4e682d1d
Karina-Melissa Constandache <karinaconstandache12@gmail.com>
2026-08-05 11:53:45 +0300

    chore: add migration to include OriginalValuesJson and AutoResolvedByImportHistoryId in DataChangeRequests

---
+++ SyncApp26/SyncApp26.Infrastructure/Migrations/20260805082040_AddAutoResolvedByImportHistoryIdToDataChangeRequest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SyncApp26.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAutoResolvedByImportHistoryIdToDataChangeRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "AutoResolvedByImportHistoryId",
+                table: "DataChangeRequests",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataChangeRequests_AutoResolvedByImportHistoryId",
+                table: "DataChangeRequests",
+                column: "AutoResolvedByImportHistoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DataChangeRequests_ImportHistories_AutoResolvedByImportHistoryId",
+                table: "DataChangeRequests",
+                column: "AutoResolvedByImportHistoryId",
+                principalTable: "ImportHistories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DataChangeRequests_ImportHistories_AutoResolvedByImportHistoryId",
+                table: "DataChangeRequests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DataChangeRequests_AutoResolvedByImportHistoryId",
+                table: "DataChangeRequests");
+
+            migrationBuilder.DropColumn(
+                name: "AutoResolvedByImportHistoryId",
+                table: "DataChangeRequests");
+        }
+    }
+}
